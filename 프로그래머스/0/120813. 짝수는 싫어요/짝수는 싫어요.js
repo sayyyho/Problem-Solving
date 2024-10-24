7cf33d04a0f41fee7bf25ac99903759bf0eb2e15
Seho Park <34148750+sayyyho@users.noreply.github.com>
2024-10-24 14:10:01 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,3 +1,1 @@
-const solution = n => {
-    return new Array(n+1).fill(0).reduce((acc, _, idx) => idx % 2 === 1 ? [...acc, idx] : [...acc], [])
-}
+const solution = n => Array(n+1).fill(0).reduce((acc, _, idx) => idx % 2 === 1 ? [...acc, idx] : [...acc], [])
